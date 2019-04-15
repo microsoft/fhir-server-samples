8d9befcee43f8dcdee187ada5d3fc3cc6ef655df
Michael Hansen <mihansen@microsoft.com>
2019-04-15 06:53:54 -0700

    Started work on Resource Module

---
+++ src/FhirDashboardJS/js/ResourceModule.js
@@ -0,0 +1,53 @@
+class ResourceModule
+{
+    constructor(resourceModal, configAuth)
+    {
+        this.resourceModal = resourceModal;
+        this.configAuth = configAuth;
+    }
+
+    show(resourceUrl)
+    {
+        var resMod = this;
+
+        // TODO set content-type to JSON
+        // Get actual FHIR Resource
+        // Set width of modal
+
+        this.configAuth.getFhirServerAccessInfo(function (fhirServerUrl, accessToken) {
+            var fullQuery = resourceUrl; // fhirServerUrl + resourceUrl;
+            $.ajax(
+                {
+                    type: 'GET',
+                    url: fullQuery,
+
+                    beforeSend: function (xhdr) {
+                        xhdr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+                    },
+                    success: function(data, status) {
+                        var markup = `
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                        <h4 id="resource-modal-title" class="modal-title">${resourceUrl}</h4>
+                                    </div>
+                                    <div id="resource-model-body" class="modal-body">
+                                        <pre>${JSON.stringify(data, null, 2)}</pre>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        `;
+            
+                        resMod.resourceModal.html(markup);
+                        resMod.resourceModal.modal('show');
+                    }
+                }
+            );
+        });
+
+    }
+}
