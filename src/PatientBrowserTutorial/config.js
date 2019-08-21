99ba7971bc4d38073722032a64274359cff733a6
Gina Lee <gi.na.lee@duke.edu>
2019-08-21 14:33:36 -0700

    revisions made based on michael's code review

---
+++ src/PatientBrowserTutorial/config.js
@@ -0,0 +1,6 @@
+var config = {
+    clientId: "<client id>",
+    authority: "https://login.microsoftonline.com/<TENANT-ID>",
+    fhirServerUrl: "https://myserver.azurehealthcareapis.com",
+    scopes: [ "https://azurehealthcareapis.com/user_impersonation" ]
+}
---
+++ src/PatientBrowserTutorial/config.local.js
@@ -0,0 +1,6 @@
+var config = {
+    clientId: "93498c57-9405-442d-bd5e-04afd4687ed0",
+    authority: "https://login.microsoftonline.com/5f104a95-0997-4a21-9d39-3892d61651a3",
+    fhirServerUrl: "https://matjazl-fhir.azurehealthcareapis.com",
+    scopes: [ "https://azurehealthcareapis.com/user_impersonation" ]
+}
